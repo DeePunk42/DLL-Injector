70ccabb475186146f3608054e49ff956898f562f
DeePunk42 <799138673@qq.com>
2024-01-23 22:13:08 +0800

    first commit

---
+++ DllInjection.cpp
@@ -0,0 +1,137 @@
+﻿#include "windows.h"
+#include "tchar.h"
+#include "stdio.h"
+#include "tlhelp32.h"
+#include "cstdio"
+
+#define targetFile "crackme.exe"
+#define targetDllPATH "C:\\Users\\DeePunk\\source\\repos\\EvilDll\\x64\\Debug\\EvilDll.dll"
+#define targetDll "EvilDll.dll"
+
+DWORD FindProcess(TCHAR*  name) {
+    PROCESSENTRY32 entry;
+    entry.dwSize = sizeof(PROCESSENTRY32);
+
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+    if (Process32First(snapshot, &entry) == TRUE)
+    {
+        while (Process32Next(snapshot, &entry) == TRUE)
+        {
+            TCHAR processName[0x30];
+            _tcscpy_s(processName, 0x30, entry.szExeFile);
+
+            if (_tcscmp(name, processName)==0)
+            {
+                _tprintf(L"PID detected: %d\n", entry.th32ProcessID);
+                CloseHandle(snapshot);
+                return entry.th32ProcessID;
+            }
+        }
+    }
+
+    CloseHandle(snapshot);
+
+    return -1;
+}
+
+BOOL InjectDll(LPCTSTR szDllPath)
+{
+    HANDLE hProcess = NULL, hThread = NULL;
+    HMODULE hMod = NULL;
+    LPVOID pRemoteBuf = NULL;
+
+    DWORD dwBufSize = (DWORD)(_tcslen(szDllPath) + 1) * sizeof(TCHAR);
+    LPTHREAD_START_ROUTINE pThreadProc;
+
+    TCHAR target[] = _T(targetFile);
+    if (!(hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, FindProcess(target))))
+    {
+        _tprintf(L"OpenProcess failed!!!\n");
+        return FALSE;
+    }
+
+    pRemoteBuf = VirtualAllocEx(hProcess, NULL, dwBufSize, MEM_COMMIT, PAGE_READWRITE);
+
+    WriteProcessMemory(hProcess, pRemoteBuf, (LPVOID)szDllPath, dwBufSize, NULL);
+
+    _tprintf(L"Write Path Successfully:%p\n", pRemoteBuf);
+
+    hMod = GetModuleHandle(L"kernel32.dll");
+    pThreadProc = (LPTHREAD_START_ROUTINE)GetProcAddress(hMod, "LoadLibraryW");
+
+    _tprintf(L"Get Address Successfully:%p\n", pThreadProc);
+
+
+    hThread = CreateRemoteThread(hProcess, NULL, 0, pThreadProc, pRemoteBuf, 0, NULL);
+    WaitForSingleObject(hThread, INFINITE);
+
+    CloseHandle(hThread);
+    CloseHandle(hProcess);
+
+    return TRUE;
+}
+
+BOOL EjectDll(LPCTSTR szDllName)
+{
+    BOOL bMore = FALSE, bFound = FALSE;
+    HANDLE hSnapshot, hProcess, hThread;
+    HMODULE hModule = NULL;
+    MODULEENTRY32 me = { sizeof(me) };
+    LPTHREAD_START_ROUTINE pThreadProc;
+
+    TCHAR target[] = _T(targetFile);
+    DWORD pid = FindProcess(target);
+    hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pid);
+
+    bMore = Module32First(hSnapshot, &me);
+    for (; bMore; bMore = Module32Next(hSnapshot, &me))
+    {
+        if (!_tcsicmp((LPCTSTR)me.szModule, szDllName) ||
+            !_tcsicmp((LPCTSTR)me.szExePath, szDllName))
+        {
+            bFound = TRUE;
+            break;
+        }
+    }
+
+    if (!bFound)
+    {
+        CloseHandle(hSnapshot);
+        return FALSE;
+    }
+
+    if (!(hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid)))
+    {
+        _tprintf(L"OpenProcess(%d) failed!!! [%d]\n", pid, GetLastError());
+        return FALSE;
+    }
+
+    hModule = GetModuleHandle(L"kernel32.dll");
+    pThreadProc = (LPTHREAD_START_ROUTINE)GetProcAddress(hModule, "FreeLibrary");
+    hThread = CreateRemoteThread(hProcess, NULL, 0,
+        pThreadProc, me.modBaseAddr,
+        0, NULL);
+    WaitForSingleObject(hThread, INFINITE);
+
+    CloseHandle(hThread);
+    CloseHandle(hProcess);
+    CloseHandle(hSnapshot);
+
+    return TRUE;
+}
+
+int _tmain(int argc, TCHAR* argv[])
+{
+    TCHAR DLL_PATH[] = _T(targetDllPATH);
+    TCHAR DLL[] = _T(targetDll);
+
+    if (InjectDll(DLL_PATH))
+        _tprintf(L"Inject Dll success!!!\n");
+
+    getchar();
+    if (EjectDll(DLL_PATH))
+        _tprintf(L"Eject Dll success!!!\n");
+    getchar();
+    return 0;
+}
---
+++ EvilDll.cpp
@@ -0,0 +1,76 @@
+﻿#include "pch.h"
+#include "Windows.h"
+#include "tchar.h"
+#include "stdio.h"
+#include "stdint.h"
+
+char* hookend;
+
+void hookFunction() {
+    printf("正确");
+    __asm {
+        mov rax, hookend
+        jmp rax
+    }
+
+}
+
+int StartHook() {
+    DWORD  oldProtect;
+    BYTE NewCode[12];
+    HMODULE base = GetModuleHandle(L"crackme.exe");
+    HMODULE dllbase = GetModuleHandle(L"EvilDll.dll");
+    printf("start hook\nbase:%p\n", base);
+    printf("dllbase:%p\n", dllbase);
+    char* target = (char*)base + 0x192b;
+    hookend = (char*)base + 0x1949;
+    printf("target:%p\n", target);
+    NewCode[0] = 0x48;
+    NewCode[1] = 0xB8;
+    uint64_t hookptr = (uint64_t)hookFunction;
+    printf("hook:%p\n", hookptr);
+    *(uint64_t*)&NewCode[2] = hookptr;
+    NewCode[10] = 0xFF;
+    NewCode[11] = 0xE0;
+
+    VirtualProtect(target, 12, PAGE_EXECUTE_READWRITE, &oldProtect);
+    memcpy(target, NewCode, 12);
+
+    return 0;
+}
+/*
+int StartHook() {
+    DWORD  oldProtect;
+    HMODULE base = GetModuleHandle(L"crackme.exe");
+    printf("start hook\nbase:%p\n", base);
+    char* target = (char*)base + 0x18e7;
+    printf("target:%p\n", target);
+    VirtualProtect(target, 5, PAGE_EXECUTE_READWRITE, &oldProtect);
+    *(DWORD*)(target) = 0x90909090;
+    return 0;
+}
+*/
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        OutputDebugString(L"DLL Inject Success");
+        MessageBox(NULL, L"DLL Injected", L"DLL Injected", MB_OK);
+        StartHook();
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        OutputDebugString(L"DLL free Sucess");
+        MessageBox(NULL, L"DLL freed", L"DLL freed", MB_OK);
+        break;
+    }
+    return TRUE;
+}
